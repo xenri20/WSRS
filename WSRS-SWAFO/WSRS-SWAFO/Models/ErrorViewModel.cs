1d7638d241a27ce159e50bd10fb52220aa0c3e49
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-10-15 21:15:00 +0800

    Initial MVC

---
+++ WSRS-SWAFO/WSRS-SWAFO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WSRS_SWAFO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
