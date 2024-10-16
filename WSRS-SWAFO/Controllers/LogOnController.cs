fb853d8d11261e3330e8e026c2d17412b16b26ba
xenri20 <skc1615@dlsud.edu.ph>
2024-10-17 05:37:50 +0800

    Added a Log On Controller

---
+++ WSRS-SWAFO/Controllers/LogOnController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WSRS_SWAFO.Controllers
+{
+    public class LogOnController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
