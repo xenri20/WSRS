fbcc6f1c36e700fe98c9a58ec7671f119cdae597
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-10-17 22:06:25 +0800

    Introduce roles, data seeding for users and roles, and make password requirement lighter

--- WSRS-SWAFO/Program.cs
+++ WSRS-SWAFO/Program.cs
@@ -3,35 +3,102 @@ using Microsoft.EntityFrameworkCore;
 using WSRS_SWAFO.Areas.Identity.Data;
 using WSRS_SWAFO.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("AuthDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthDbContextConnection' not found.");
-
-// Add services to the container.
-builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<AuthDbContext>();
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static async Task Main(String[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var connectionString = builder.Configuration.GetConnectionString("AuthDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthDbContextConnection' not found.");
+        
+        // Add services to the container.
+        builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(connectionString));
+        builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+            {
+                // Temporary definition of password requirements for testing
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<AuthDbContext>();
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
+        
+        var app = builder.Build();
+        
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        
+        app.UseRouting();
+        
+        app.UseAuthorization();
+        
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+        app.MapRazorPages();
+
+        await CreateRoles(app.Services);
+        await CreateAdmin(app.Services);
+
+        app.Run();
+    }
+
+    private static async Task CreateRoles(IServiceProvider serviceProvider)
+    {
+        using (var scope = serviceProvider.CreateScope())
+        {
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var roleNames = new[] { "Admin", "Manager", "Member" };
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(roleName);
+
+                if (!roleExist)
+                {
+                    //create the roles and seed them to the database
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+        }
+    }
+
+    private static async Task CreateAdmin(IServiceProvider serviceProvider)
+    {
+        using (var scope = serviceProvider.CreateScope())
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            string adminEmail = "admin@test.com";
+            string adminPassword = "admin123";
+
+            if (await userManager.FindByEmailAsync(adminEmail) == null)
+            {
+                var user = new ApplicationUser();
+
+                user.FirstName = "N/A";
+                user.LastName = "N/A";
+                user.UserName = adminEmail;
+                user.Email = adminEmail;
+
+                await userManager.CreateAsync(user, adminPassword);
+
+                await userManager.AddToRoleAsync(user,"Admin");
+            }
+        }
+    }
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.Run();
