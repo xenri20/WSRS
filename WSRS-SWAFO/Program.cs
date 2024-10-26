6a49cb49552c49a6aa2fef215520246e45141e47
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-10-26 23:00:32 +0800

    Minor tweak to point exception handler page the right way

--- WSRS-SWAFO/Program.cs
+++ WSRS-SWAFO/Program.cs
@@ -35,7 +35,7 @@ public class Program
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
-            app.UseExceptionHandler("/Home/Error");
+            app.UseExceptionHandler("/Dashboard/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
