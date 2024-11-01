51bf788acaf40143db8e1f96631c13af685c619d
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-11-02 00:52:10 +0800

    Change formator id from int to string

--- WSRS-SWAFO/Models/ReportPending.cs
+++ WSRS-SWAFO/Models/ReportPending.cs
@@ -9,7 +9,7 @@ namespace WSRS_SWAFO.Models
         [Key]
         public int Id { get; set; }
         [ForeignKey("ApplicationUser")]
-        public int FormatorId { get; set; }
+        public string FormatorId { get; set; }
         public string Description { get; set; }
         public DateTime CommissionDatetime { get; set; }
         [ForeignKey("Student")]
