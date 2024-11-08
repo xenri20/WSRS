028c5b75de9e0ef4c6f17a5b91e7bfcd0ebb789a
MrCLorenz <clsia0328@gmail.com>
2024-11-08 16:33:32 +0800

    Added MinorTraffic, MajorTraffic in Offense Classification and Added TrafficReportsEncoded in Models

--- WSRS-SWAFO/Data/Enum/OffenseClassification.cs
+++ WSRS-SWAFO/Data/Enum/OffenseClassification.cs
@@ -3,7 +3,9 @@ namespace WSRS_SWAFO.Data.Enum
     public enum OffenseClassification
     {
         Minor,
+        MinorTraffic,
         Major,
-        TrafficRelated
+        MajorTraffic
+        
     }
 }
---
+++ WSRS-SWAFO/Models/TrafficReportsEncoded.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WSRS_SWAFO.Models
+{
+    public class TrafficReportsEncoded
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Offense")]
+        public int OffenseId { get; set; }
+        public Offense Offense { get; set; } // Navigation Property
+
+        [ForeignKey("Student")]
+        public int StudentNumber { get; set; }
+        public Student Student { get; set; } // Navigation Property
+        public string PlateNumber { get; set; }
+        public DateTime CommissionDatetime { get; set; }
+        public string Place { get; set; }
+        public string Remarks { get; set; }
+        public int ORNumber { get; set; }
+        public DateOnly DatePaid { get; set; }
+    }
+}
