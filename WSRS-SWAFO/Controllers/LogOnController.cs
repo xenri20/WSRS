f79e71b88c902ac756f7c3f4e797f556fc4d42de
xenri20 <skc1615@dlsud.edu.ph>
2024-11-09 14:28:04 +0800

    Implement Redirects and Network Checker .IsOnline via Network Helper Class

--- WSRS-SWAFO/Controllers/LogOnController.cs
+++ WSRS-SWAFO/Controllers/LogOnController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WSRS_SWAFO.Controllers
 {
     public class LogOnController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
+            bool isOnline = await NetworkHelper.IsOnline();
+            ViewData["isOnline"] = isOnline;
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+
             return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult SignInWithMicrosoft()
+        {
+            var redirectUrl = Url.Action("Index", "Dashboard");
+            return Challenge(new AuthenticationProperties { RedirectUri = redirectUrl }, "AzureWSRSLogin");
         }
     }
 }
