f6026a819d59a7f4bbfda5e3dad41b7950a45b0b
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-11-13 16:35:17 +0800

    Combine users' first and last name as migration

--- WSRS-SWAFO/Migrations/ApplicationDb/20241113082424_Combine User First and Last.Designer.cs
+++ WSRS-SWAFO/Migrations/ApplicationDb/20241113082424_Combine User First and Last.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WSRS_SWAFO.Models;
 
@@ -11,9 +12,11 @@ using WSRS_SWAFO.Models;
 namespace WSRS_SWAFO.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241113082424_Combine User First and Last")]
+    partial class CombineUserFirstandLast
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
