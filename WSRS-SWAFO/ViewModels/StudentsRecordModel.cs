eaccba7141ac384dd7e70650ae63eb292fc3105e
xenri20 <skc1615@dlsud.edu.ph>
2024-12-02 03:23:34 +0800

    Created a View Model for Student Data

---
+++ WSRS-SWAFO/ViewModels/StudentsRecordModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSRS_SWAFO.ViewModels
+{
+    public class StudentsRecordModel
+    {
+        [Display(Name = "Student Number")]
+        //[Required(ErrorMessage = "Email Address is required")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "First Name")]
+        //[Required(ErrorMessage = "Password is required")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "CYS")]
+        public string Course { get; set; }
+    }
+}
