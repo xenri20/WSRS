7630e0ae00dda79cb87bbb8d5fa963093bd75db7
xenri20 <skc1615@dlsud.edu.ph>
2024-12-02 03:23:01 +0800

    Created a Encode Controller for Index

---
+++ WSRS-SWAFO/Controllers/EncodeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WSRS_SWAFO.Controllers
+{
+    public class EncodeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
