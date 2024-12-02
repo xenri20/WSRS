6a2f19110c2a40ce1b63e32166d0214ebc3f31c7
xenri20 <skc1615@dlsud.edu.ph>
2024-12-02 15:22:22 +0800

    Created Controllers for each encode tab function

---
+++ WSRS-SWAFO/Controllers/PendingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WSRS_SWAFO.Controllers
+{
+    public class PendingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WSRS-SWAFO/Controllers/StudentViolationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WSRS_SWAFO.Controllers
+{
+    public class StudentViolationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
