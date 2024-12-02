2a5865567863ed981cbc7466b4a60b6e4a921f16
xenri20 <skc1615@dlsud.edu.ph>
2024-12-03 00:45:17 +0800

    Created student violation js for form pagination

---
+++ WSRS-SWAFO/wwwroot/js/studentViolation.js
@@ -0,0 +1,35 @@
+﻿function formInitializer() {
+    document.querySelector("#first-page").style.display = "flex";
+    document.querySelector("#create-new").style.display = "none";
+    document.querySelector("#record-violation").style.display = "none";
+}
+
+document.querySelectorAll("#preview-records tbody tr").forEach(row => {
+    row.addEventListener("click", function () {
+        const isSelected = this.classList.contains("selected");
+        document.querySelectorAll("#preview-records tbody tr").forEach(r => r.classList.remove("selected")); 
+        if (!isSelected) {
+            this.classList.add("selected");  
+        }
+    });
+});
+
+document.querySelector("#first-page .btn-next").addEventListener("click", function () {
+    document.querySelector("#first-page").style.display = "none";
+    document.querySelector("#create-new").style.display = "inline-flex";
+});
+
+document.querySelector("#create-new .btn-back").addEventListener("click", function () {
+    document.querySelector("#first-page").style.display = "flex";
+    document.querySelector("#create-new").style.display = "none";
+})
+
+document.querySelector("#create-new .btn-next").addEventListener("click", function () {
+    document.querySelector("#create-new").style.display = "none";
+    document.querySelector("#record-violation").style.display = "inline-flex";
+})
+
+document.querySelector("#record-violation .btn-back").addEventListener("click", function () {
+    document.querySelector("#create-new").style.display = "inline-flex";
+    document.querySelector("#record-violation").style.display = "none";
+})
