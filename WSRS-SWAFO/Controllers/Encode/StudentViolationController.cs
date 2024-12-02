21b285be9d6e59a027cb97fcfebd734be06ac396
xenri20 <skc1615@dlsud.edu.ph>
2024-12-03 00:40:01 +0800

    Added a search function

--- WSRS-SWAFO/Controllers/Encode/StudentViolationController.cs
+++ WSRS-SWAFO/Controllers/Encode/StudentViolationController.cs
@@ -20,5 +20,32 @@ namespace WSRS_SWAFO.Controllers.Encode
             var students = await _student.Students.ToListAsync();
             return View("~/Views/Encode/StudentViolation/Index.cshtml", students);
         }
+
+        public IActionResult Search(string searchStudent)
+        {
+            var studentsQuery = _student.Students.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchStudent))
+            {
+                if (int.TryParse(searchStudent, out int studentNumber))
+                {
+                    studentsQuery = studentsQuery.Where(s => s.StudentNumber == studentNumber);
+                }
+                else
+                {
+                    studentsQuery = studentsQuery.Where(s => s.LastName.Contains(searchStudent) || s.FirstName.Contains(searchStudent));
+                }
+            }
+
+            // Map the query to StudentsRecordModel
+            var result = studentsQuery.Select(s => new StudentsRecordModel
+            {
+                StudentNumber = s.StudentNumber, // Assuming the view model requires it as a string
+                LastName = s.LastName,
+                FirstName = s.FirstName
+            });
+
+            return View(result); // Pass the mapped IQueryable to the view
+        }
     }
 }
