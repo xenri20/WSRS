d47397def896c8c206492bfe537a2366817479ae
xenri20 <skc1615@dlsud.edu.ph>
2024-12-03 12:39:07 +0800

    Create Data Annotation for DropDown List View

--- WSRS-SWAFO/Data/Enum/OffenseClassification.cs
+++ WSRS-SWAFO/Data/Enum/OffenseClassification.cs
@@ -1,11 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WSRS_SWAFO.Data.Enum
 {
     public enum OffenseClassification
     {
         Minor,
+        [Display(Name = "Minor Traffic")]
         MinorTraffic,
         Major,
+        [Display(Name = "Major Traffic")]
         MajorTraffic
-        
     }
 }
