30cc50929bf11cc5f29a0a9c213b48631ceba2e9
xenri20 <skc1615@dlsud.edu.ph>
2024-12-06 23:43:06 +0800

    Create ViewModel for Querying Data and Posting Student Data

---
+++ WSRS-SWAFO/ViewModels/CreateStudentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WSRS_SWAFO.ViewModels
+{
+    public class CreateStudentViewModel
+    {
+        public StudentsRecordModel NewStudent { get; set; }
+        public IQueryable<StudentsRecordModel> ExistingStudents { get; set; }
+    }
+}
