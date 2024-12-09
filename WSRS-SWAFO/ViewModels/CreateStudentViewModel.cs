a1dd022d29b930013de5c5c1660048631ba77b20
xenri20 <skc1615@dlsud.edu.ph>
2024-12-09 17:35:23 +0800

    Implement Validate Never Data Annotation for Existing Students

--- WSRS-SWAFO/ViewModels/CreateStudentViewModel.cs
+++ WSRS-SWAFO/ViewModels/CreateStudentViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace WSRS_SWAFO.ViewModels
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace WSRS_SWAFO.ViewModels
 {
     public class CreateStudentViewModel
     {
         public StudentsRecordModel NewStudent { get; set; }
+        [ValidateNever]
         public IQueryable<StudentsRecordModel> ExistingStudents { get; set; }
     }
 }
