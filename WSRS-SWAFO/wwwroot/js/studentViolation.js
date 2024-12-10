ca367a36af71acfd6ba0b9a85eaee80e44dd26ff
xenri20 <skc1615@dlsud.edu.ph>
2024-12-10 11:57:57 +0800

    Trying to introduce JQuery Selector

--- WSRS-SWAFO/wwwroot/js/studentViolation.js
+++ WSRS-SWAFO/wwwroot/js/studentViolation.js
@@ -11,19 +11,26 @@ for (btnLink of btnLinks) {
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".encode-nav").classList.add("active");
     document.querySelector(".student-encode").classList.add("active");
-    document.querySelector("#first-page").style.display = "flex";
-    document.querySelector("#create-new").style.display = "none";
-    document.querySelector("#record-violation").style.display = "none";
+    //document.querySelector("#first-page").style.display = "flex";
+    //document.querySelector("#create-new").style.display = "none";
+    //document.querySelector("#record-violation").style.display = "none";
+    $('#first-page').show();
+    $('#record-violation').hide();
+    $('#create-new').hide();
 });
 
 document.querySelector("#first-page .btn-create").addEventListener("click", function () {
-    document.querySelector("#first-page").style.display = "none";
-    document.querySelector("#create-new").style.display = "inline-flex";
+    //document.querySelector("#first-page").style.display = "none";
+    //document.querySelector("#create-new").style.display = "inline-flex";
+    $('#first-page').hide();
+    $('#create-new').show();
 });
 
 document.querySelector("#create-new .btn-back").addEventListener("click", function () {
-    document.querySelector("#first-page").style.display = "flex";
-    document.querySelector("#create-new").style.display = "none";
+    //document.querySelector("#first-page").style.display = "flex";
+    //document.querySelector("#create-new").style.display = "none";
+    $('#first-page').show();
+    $('#create-new').none();
 })
 
 document.querySelector("#create-new").addEventListener("submit", function (e) {
@@ -36,7 +43,22 @@ document.querySelector("#create-new").addEventListener("submit", function (e) {
     document.querySelector("#record-violation").style.display = "inline-flex";
 });
 
+document.querySelector(".btn-select").addEventListener("click", function () {
+    //document.querySelector("#first-page").style.display = "none";
+    //document.querySelector("#record-violation").style.display = "none";
+    $('#first-page').hide();
+    $('#record-violation').show();
+});
+
+function saveStudentData() {
+
+    $('#first-page').hide();
+    $('#record-violation').show();
+}
+
 document.querySelector("#record-violation .btn-back").addEventListener("click", function () {
-    document.querySelector("#create-new").style.display = "inline-flex";
-    document.querySelector("#record-violation").style.display = "none";
+    //document.querySelector("#create-new").style.display = "inline-flex";
+    //document.querySelector("#record-violation").style.display = "none";
+    $('#create-new').show();
+    $('#record-violation').none();
 })
