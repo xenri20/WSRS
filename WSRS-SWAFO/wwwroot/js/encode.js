3ebe03717a93ae5eed71285ad71d0f698c13db37
xenri20 <skc1615@dlsud.edu.ph>
2024-12-12 23:21:44 +0800

    Add encode js

---
+++ WSRS-SWAFO/wwwroot/js/encode.js
@@ -0,0 +1,13 @@
+﻿var btnLinks = document.querySelectorAll("#category-function a")
+for (btnLink of btnLinks) {
+    btnLink.addEventListener("click", function (e) {
+        for (eachCategory of btnLinks) {
+            eachCategory.classList.remove("active");
+        }
+        e.currentTarget.classList.add("active");
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".encode-nav").classList.add("active");
+});
