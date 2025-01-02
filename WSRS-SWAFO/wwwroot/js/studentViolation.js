af1c33676e6c5fc6ef9575a2dd94c4197b3e5cfb
xenri20 <skc1615@dlsud.edu.ph>
2025-01-03 00:54:51 +0800

    Omit, Fix: AJAX for Form Action that devoids form submission

--- WSRS-SWAFO/wwwroot/js/studentViolation.js
+++ WSRS-SWAFO/wwwroot/js/studentViolation.js
@@ -11,16 +11,3 @@ document.querySelector("#create-new").addEventListener("submit", function (e) {
     this.style.display = "none";
     document.querySelector("#record-violation").style.display = "inline-flex";
 });
-
-document.getElementById('create-new').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    var studentNumber = document.querySelector('[name="StudentNumber"]').value;
-    var firstName = document.querySelector('[name="FirstName"]').value;
-    var lastName = document.querySelector('[name="LastName"]').value;
-
-    var formAction = '/Encode/EncodeStudentViolation';
-    formAction += `?studentNumber=${studentNumber}&firstName=${firstName}&lastName=${lastName}`;
-
-    window.location.href = formAction;
-});
