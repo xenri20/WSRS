b2a540a95b25660bf4644073b529bb282e4191be
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-03-15 18:56:50 +0800

    feat: add autocomplete to report descriptions

---
+++ WSRS-SWAFO/Controllers/OffensesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WSRS_SWAFO.Data;
+using WSRS_SWAFO.Models;
+
+namespace WSRS_SWAFO.Controllers
+{
+    public class OffensesController : Controller
+    {
+        private readonly ILogger<OffensesController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public OffensesController(ApplicationDbContext context, ILogger<OffensesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("api/[controller]/GetAllDescriptions")]
+        [HttpGet]
+        [Produces("application/json")]
+        public async Task<ActionResult<ReportEncoded>?> GetAllPreviousDescriptions()
+        {
+            var reports = await _context.ReportsEncoded
+                .Select(r => r.Description)
+                .Distinct()
+                .ToListAsync();
+
+            if (!reports.Any())
+            {
+                return null;
+            }
+
+            return Ok(reports);
+        }
+    }
+}
