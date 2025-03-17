23045c752830ccf376578f43d645fc2b15f1c12b
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-03-17 20:20:36 +0800

    fix: change traffic CommissionDate SQL type to date only

--- WSRS-SWAFO/Migrations/20250317121459_AlterTypeCommissionDatetime.Designer.cs
+++ WSRS-SWAFO/Migrations/20250317121459_AlterTypeCommissionDatetime.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WSRS_SWAFO.Data;
 
@@ -11,9 +12,11 @@ using WSRS_SWAFO.Data;
 namespace WSRS_SWAFO.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250317121459_AlterTypeCommissionDatetime")]
+    partial class AlterTypeCommissionDatetime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
