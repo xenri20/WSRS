2252657b16f82c06a57b2efa32b5bf683c84d3bf
xenri20 <skc1615@dlsud.edu.ph>
2025-04-09 10:55:15 +0800

    feat: api routing to student/student

--- WSRS-Student/Controllers/StudentController.cs
+++ WSRS-Student/Controllers/StudentController.cs
@@ -5,6 +5,7 @@ using WSRS_Student.Data;
 namespace WSRS_Student.Controllers
 {
     [Authorize]
+    [Route("student/student")]
     public class StudentController : Controller
     {
         public StudentController(ApplicationDbContext context)
