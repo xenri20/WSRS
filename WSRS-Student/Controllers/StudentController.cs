ac94f755ea2997abee04a8c674c361afa298291f
xenri20 <skc1615@dlsud.edu.ph>
2025-04-09 10:55:57 +0800

    feat: violations api

--- WSRS-Student/Controllers/StudentController.cs
+++ WSRS-Student/Controllers/StudentController.cs
@@ -15,8 +15,25 @@ namespace WSRS_Student.Controllers
 
         public StudentController(ApplicationDbContext context)
         {
-            // 
+            _context = context;
         }
 
+        [HttpGet("")]
+        public IActionResult Violations()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var student = _context.Students
+                .Include(s => s.ReportsEncoded)
+                .Include(s => s.TrafficReportsEncoded)
+                .FirstOrDefaultAsync(s => s.IdentityUserId == userId);
+
+            if (student == null)
+            {
+                return NotFound(); // Or redirect to an error page
+            }
+
+            return View(student);
+        }
     }
 }
