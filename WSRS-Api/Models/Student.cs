d47bf20c763bd64407075e5f16b0acab3060aad9
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-13 23:05:12 +0800

    feat: add student model

---
+++ WSRS-Api/Models/Student.cs
@@ -0,0 +1,8 @@
+namespace WSRS_Api.Models;
+
+public class Student
+{
+    public int StudentNumber { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
