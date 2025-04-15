572c727d25343997d9f1e528e0acb84c39193d39
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-15 20:58:44 +0800

    chore: remove all unneeded models

--- WSRS-Student/Data/AzureDbContext.cs
+++ WSRS-Student/Data/AzureDbContext.cs
@@ -7,12 +7,5 @@ namespace WSRS_Student.Data
     public class AzureDbContext : IdentityDbContext<ApplicationUser>
     {
         public AzureDbContext(DbContextOptions<AzureDbContext> options) : base(options) { }
-
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Offense> Offenses { get; set; }
-        public DbSet<College> Colleges { get; set; }
-        public DbSet<ReportEncoded> ReportsEncoded { get; set; }
-        public DbSet<TrafficReportsEncoded> TrafficReportsEncoded { get; set; }
-        public DbSet<ReportPending> ReportsPending { get; set; }
     }
 }
