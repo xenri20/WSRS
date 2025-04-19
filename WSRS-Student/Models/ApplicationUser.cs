51181b4ed4bb0da55f35f3ebb1131101b6e2d0ff
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-19 23:52:34 +0800

    chore: remove unused data annotations

--- WSRS-Student/Models/ApplicationUser.cs
+++ WSRS-Student/Models/ApplicationUser.cs
@@ -1,12 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace WSRS_Student.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        [Required(ErrorMessage = "Password is required")]
-        [DataType(DataType.Password)]
         public string FullName { get; set; } 
         public string StudentNumber { get; set; }
     }
