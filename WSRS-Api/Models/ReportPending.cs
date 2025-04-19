35c15628188d83510d1fa524180102ce4b011de1
xenri20 <kennethsoria20@gmail.com>
2025-04-20 02:55:47 +0800

    feat: added report pending model

---
+++ WSRS-Api/Models/ReportPending.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WSRS_Api.Models;
+
+namespace WSRS_Api.Models
+{
+    public class ReportPending
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string FormatorId { get; set; } // Navigation Property
+
+        public string Description { get; set; }
+        public DateTime CommissionDatetime { get; set; }
+
+        [ForeignKey("Student")]
+        public int StudentNumber { get; set; }
+        public Student Student { get; set; } // Navigation Property
+    }
+}
