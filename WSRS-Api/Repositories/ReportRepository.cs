072622fd1ed8e8e4480d29884ccabe02769041ab
xenri20 <kennethsoria20@gmail.com>
2025-04-20 03:27:33 +0800

    feat: setup report pending object and saving changes to db

--- WSRS-Api/Repositories/ReportRepository.cs
+++ WSRS-Api/Repositories/ReportRepository.cs
@@ -1,4 +1,10 @@
-﻿namespace WSRS_Api.Repositories
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using WSRS_Api.Data;
+using WSRS_Api.Dtos;
+using WSRS_Api.Models;
+
+namespace WSRS_Api.Repositories
 {
     public class ReportRepository
     {
@@ -11,5 +17,18 @@
             _context = context;
         }
 
+        public Task<ReportPendingDto> PostStudentViolation(string FormatorId, string Description, int StudentNumber)
+        {
+            var reportPending = new ReportPendingDto
+                {
+                    FormatorId = FormatorId,
+                    Description = Description,
+                    CommissionDatetime = DateTime.Now,
+                    StudentNumber = StudentNumber
+                };
+
+                _context.ReportPending.Any(reportPending);
+                _context.SaveChanges();
+        }
     }
 }
