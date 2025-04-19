f3bc4aa9c9d1db14db31a0b945120e957deb960a
xenri20 <kennethsoria20@gmail.com>
2025-04-20 02:55:27 +0800

    feat: added a report controller

---
+++ WSRS-Api/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WSRS_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        // POST api/<ReportController>
+        [HttpPost]
+        public IActionResult PostStudentViolation()
+        {
+        }
+    }
+}
