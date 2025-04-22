af2d7a0afd1ae7dc2773b101a5a1a87e0a9085b3
xenri20 <kennethsoria20@gmail.com>
2025-04-22 21:28:54 +0800

    feat: append reportspending to all reports

--- WSRS-Api/Dtos/AllReportsDto.cs
+++ WSRS-Api/Dtos/AllReportsDto.cs
@@ -4,4 +4,5 @@ public class AllReportsDto
 {
     public IEnumerable<ReportEncodedDto>? Violations { get; set; }
     public IEnumerable<TrafficReportEncodedDto>? TrafficViolations { get; set; }
+    public IEnumerable<ReportsPendingDto>? ReportsPending { get; set; }
 }
