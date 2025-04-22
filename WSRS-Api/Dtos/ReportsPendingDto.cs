ebac1f1b1855959a3752ac9b8af0f1be5c928292
xenri20 <kennethsoria20@gmail.com>
2025-04-22 21:26:23 +0800

    refactor: update to reportspendingdto

---
+++ WSRS-Api/Dtos/ReportsPendingDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WSRS_Api.Dtos
+{
+    public class ReportsPendingDto
+    {
+        [Required]
+        public int FormatorId { get; set; }
+        [Required]
+        public string Formator { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateOnly ReportDate { get; set; }
+        [Required]
+        public int StudentNumber { get; set; }
+        [Required]
+        public bool IsArchived { get; set; }
+    }
+}
