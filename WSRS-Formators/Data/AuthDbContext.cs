e6ab6b25b0e3ea5f4de3dadc339321368c090cf8
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-23 20:11:36 +0800

    feat: update formator db context

--- WSRS-Formators/Data/AuthDbContext.cs
+++ WSRS-Formators/Data/AuthDbContext.cs
@@ -7,17 +7,6 @@ namespace WSRS_Formators.Data
 {
     public class AuthDbContext : IdentityDbContext<FormatorUser>
     {
-        public AuthDbContext(DbContextOptions<AuthDbContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Offense> Offenses { get; set; }
-        public DbSet<FormatorUser> FormatorUser { get; set; }
-        public DbSet<College> Colleges { get; set; }
-        public DbSet<ReportEncoded> ReportsEncoded { get; set; }
-        public DbSet<TrafficReportsEncoded> TrafficReportsEncoded { get; set; }
-        public DbSet<ReportPending> ReportsPending { get; set; }
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
     }
 }
--- WSRS-Formators/Migrations/20250423120821_InitialCreate.Designer.cs
+++ WSRS-Formators/Migrations/20250423120821_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WSRS_Formators.Data;
 
@@ -11,9 +12,11 @@ using WSRS_Formators.Data;
 namespace WSRS_Formators.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250423120821_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
