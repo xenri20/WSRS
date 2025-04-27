7889fed5a61b98d79d26540ebeff9fb59365fda1
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-27 22:56:21 +0800

    feat: add toast functionality
    Just add TempData["Result"] to show the message

--- WSRS-SWAFO/Controllers/EncodeController.cs
+++ WSRS-SWAFO/Controllers/EncodeController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
 using WSRS_SWAFO.Data;
 using WSRS_SWAFO.Data.Enum;
 using WSRS_SWAFO.Models;
@@ -81,12 +80,6 @@ namespace WSRS_SWAFO.Controllers
                 {
                     studentsQuery = studentsQuery.Where(s => s.LastName.Contains(searchStudent) || s.FirstName.Contains(searchStudent));
                 }
-            var toastMessage = new ToastViewModel
-            {
-                Title = "Success",
-                Message = "Registered a new student in the system."
-            };
-            TempData["Result"] = JsonConvert.SerializeObject(toastMessage);
 
                 // Once existed, compiler creates table query
                 var ExistingStudent = studentsQuery.Select(s => new StudentRecordViewModel
---
+++ WSRS-SWAFO/ViewModels/ToastViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WSRS_SWAFO.ViewModels
+{
+    public class ToastViewModel
+    {
+        public string? Title { get; set; }
+        public string Message { get; set; }
+        public string? CssClassName { get; set; }
+    }
+}
