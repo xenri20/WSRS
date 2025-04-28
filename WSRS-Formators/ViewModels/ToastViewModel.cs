bf8c0d8674372038268b4eeb017427cfd4140e3e
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-28 21:51:48 +0800

    feat: add toast functionality
    Just add TempData["Result"] to show the message

---
+++ WSRS-Formators/ViewModels/ToastViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WSRS_Formators.ViewModels
+{
+    public class ToastViewModel
+    {
+        public string? Title { get; set; }
+        public string Message { get; set; }
+        public string? CssClassName { get; set; }
+    }
+}
