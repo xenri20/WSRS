e0957fbc6e272fc48b4db16765ed80adbd9bca61
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-29 17:11:35 +0800

    feat: add archived reports pending in context

--- WSRS-SWAFO/Data/ApplicationDbContext.cs
+++ WSRS-SWAFO/Data/ApplicationDbContext.cs
@@ -12,13 +12,16 @@ namespace WSRS_SWAFO.Data
         public DbSet<Offense> Offenses { get; set; }
         public DbSet<College> College { get; set; } 
         public DbSet<ReportEncoded> ReportsEncoded { get; set; }
-        public DbSet<ReportPending> ReportsPending { get; set; }
+        public DbSet<ReportPending> ReportsPending { get; set; } // read-only replicated from Azure
         public DbSet<TrafficReportsEncoded> TrafficReportsEncoded { get; set; }
+        public DbSet<ArchivedReportPending> ArchivedReportsPending { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            
+
+            builder.Entity<ReportPending>()
+                .ToView(null); // prevents EF from trying to generate migrations for this read-only table
         }
     }
 }
