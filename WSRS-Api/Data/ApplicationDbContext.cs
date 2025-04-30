051e5f5326ac4af24f90a1a33a9d2612634872bf
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-04-30 23:47:34 +0800

    chore: add reports pending migration

--- WSRS-Api/Data/ApplicationDbContext.cs
+++ WSRS-Api/Data/ApplicationDbContext.cs
@@ -11,4 +11,18 @@ public class ApplicationDbContext : DbContext
     public DbSet<ReportEncoded> ReportsEncoded { get; set; }
     public DbSet<TrafficReportEncoded> TrafficReportsEncoded { get; set; }
     public DbSet<ReportsPending> ReportsPending { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Student>()
+            .ToView(null); // prevents EF from trying to generate migrations for this read-only table
+
+        builder.Entity<ReportEncoded>()
+            .ToView(null); // prevents EF from trying to generate migrations for this read-only table
+
+        builder.Entity<TrafficReportEncoded>()
+            .ToView(null); // prevents EF from trying to generate migrations for this read-only table
+    }
 }
