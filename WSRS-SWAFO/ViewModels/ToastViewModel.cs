e424af4c50cf61aebed69485cc3b018782d3d2d3
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-05-01 13:45:57 +0800

    feat: extend toast functionality with action button

--- WSRS-SWAFO/ViewModels/ToastViewModel.cs
+++ WSRS-SWAFO/ViewModels/ToastViewModel.cs
@@ -5,5 +5,14 @@
         public string Title { get; set; }
         public string Message { get; set; }
         public string CssClassName { get; set; }
+        public ToastButton? Button { get; set; } = null;
+    }
+
+    public class ToastButton
+    {
+        public string Name { get; set; }
+        public string? Action { get; set; }
+        public string? Controller { get; set; }
+        public Dictionary<string, object>? RouteValues { get; set; }
     }
 }
