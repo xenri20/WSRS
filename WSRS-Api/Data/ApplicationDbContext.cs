955f6cbde7fb9df49c92922ba24b23336440b063
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-05-02 12:42:27 +0800

    fix: add reference to offense in db

--- WSRS-Api/Data/ApplicationDbContext.cs
+++ WSRS-Api/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
     public DbSet<Student> Students { get; set; }
+    public DbSet<Offense> Offenses { get; set; }
     public DbSet<ReportEncoded> ReportsEncoded { get; set; }
     public DbSet<TrafficReportEncoded> TrafficReportsEncoded { get; set; }
     public DbSet<ReportsPending> ReportsPending { get; set; }
