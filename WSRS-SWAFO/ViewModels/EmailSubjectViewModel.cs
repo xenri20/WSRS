57d1cefb98f2c59c02efaa5e100ce83b2621b119
xenri20 <kennethsoria20@gmail.com>
2025-05-04 04:37:08 +0800

    feat: added emailsubjectvm

---
+++ WSRS-SWAFO/ViewModels/EmailSubjectViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WSRS_SWAFO.Models;
+
+namespace WSRS_SWAFO.ViewModels
+{
+    public class EmailSubjectViewModel
+    {
+        public string email { get; set; }
+        [ForeignKey(nameof(ReportEncoded))]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string sanction { get; set; }
+    }
+}
