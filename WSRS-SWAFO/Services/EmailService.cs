0922ca84075970e4433e8e7667d76e0a16e0e771
xenri20 <kennethsoria20@gmail.com>
2025-05-04 04:37:39 +0800

    feat: added model binding and message template to email async

--- WSRS-SWAFO/Services/EmailService.cs
+++ WSRS-SWAFO/Services/EmailService.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Options;
+﻿using DocumentFormat.OpenXml.Office2016.Presentation.Command;
+using Microsoft.Extensions.Options;
 using System.Net;
 using System.Net.Mail;
 using WSRS_SWAFO.Interfaces;
+using WSRS_SWAFO.ViewModels;
 
 namespace WSRS_SWAFO.Services
 {
@@ -14,7 +16,7 @@ namespace WSRS_SWAFO.Services
             _emailSettings = emailSettings.Value;
         }
 
-        public async Task SendEmailAsync(string email, string subject, string message)
+        public async Task SendEmailAsync(EmailSubjectViewModel emailTemplate)
         {
             using var client = new SmtpClient(_emailSettings.SmtpClientMail, _emailSettings.PortAddress)
             {
@@ -26,11 +28,30 @@ namespace WSRS_SWAFO.Services
             var mailMessage = new MailMessage
             {
                 From = new MailAddress(_emailSettings.DevEmail),
-                Subject = subject,
-                Body = message,
+                Subject = "[#" + emailTemplate.id + "] Notice of Violation for " + emailTemplate.name + " - " + emailTemplate.sanction,
+                Body = @"<!DOCTYPE html>
+                        <html>
+                        <head>
+                        <meta charset=""UTF-8"">
+                        <title>Violation Notice</title>
+                        </head>
+                        <body style=""font-family: 'Times New Roman', Times, serif; font-size: 12pt; line-height: 1.5; color: #000;"">
+                        <p>Greetings from SWAFO,</p>
+
+                        <p>We hope this message finds you well. This is to formally inform you that a violation has been recorded under your name. Kindly access your Violations Portal on the DLSU-D portal for more information.</p>
+
+                        <p>Our office is committed to upholding student discipline and fairness. We encourage you to coordinate with us at the Student Welfare and Formation Office (SWAFO) at your earliest convenience to discuss the matter and address any concerns you may have.</p>
+
+                        <p>Should you have any questions or need further clarification, please do not hesitate to visit our office at <strong>(GMH111)</strong>.</p>
+
+                        <p>With Regards,<br>
+                        <strong>Student Welfare and Formation Office (SWAFO)</strong></p>
+                        </body>
+                        </html>
+                        ",
                 IsBodyHtml = true
             };
-            mailMessage.To.Add(email);
+            mailMessage.To.Add(emailTemplate.email);
 
             await client.SendMailAsync(mailMessage);
         }
