07ae947ef641b3df1cf0e4bc51e701bb41c70b7b
xenri20 <kennethsoria20@gmail.com>
2025-05-04 04:38:10 +0800

    refactor: changed to emailsubvm

--- WSRS-SWAFO/Interfaces/IEmailSender.cs
+++ WSRS-SWAFO/Interfaces/IEmailSender.cs
@@ -1,9 +1,10 @@
 ﻿using System.Net.Mail;
+using WSRS_SWAFO.ViewModels;
 
 namespace WSRS_SWAFO.Interfaces
 {
     public interface IEmailSender
     {
-        Task SendEmailAsync(string email, string subject, string message);
+        Task SendEmailAsync(EmailSubjectViewModel emailTemplate);
     }
 }
