a99764429df80357ca58c1d3cd7cc18d0b5c685c
xenri20 <kennethsoria20@gmail.com>
2025-05-14 16:54:24 +0800

    feat: added gender enum

---
+++ WSRS-Student/Data/Enum/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSRS_Student.Data.Enum
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        [Display(Name = "Prefer Not To Say")]
+        PreferNotToSay
+    }
+}
