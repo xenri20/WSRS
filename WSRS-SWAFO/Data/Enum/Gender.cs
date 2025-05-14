545b72c5dada1d3e5cba71b822ff820bdc10b2c7
xenri20 <kennethsoria20@gmail.com>
2025-05-14 23:09:01 +0800

    feat: added enum for gender swafo project

---
+++ WSRS-SWAFO/Data/Enum/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSRS_SWAFO.Data.Enum
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        [Display(Name = "Prefer Not To Say")]
+        PreferNotToSay
+    }
+}
