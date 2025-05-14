24ba0ea0a5a88e4af85314b204ad9251c484a6ed
xenri20 <kennethsoria20@gmail.com>
2025-05-14 22:56:57 +0800

    feat: added gmc request view, vm

---
+++ WSRS-SWAFO/ViewModels/GoodMoralRequestViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WSRS_Student.Data.Enum;
+
+namespace WSRS_Student.ViewModels
+{
+    public class GoodMoralRequestViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Application Date")]
+        public DateOnly ApplicationDate { get; set; }
+        public string Purpose { get; set; }
+        public string Copies { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+        public string Course { get; set; }
+        [Display(Name = "Student Number")]
+        public int StudentNumber { get; set; }
+        public Gender Gender { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
