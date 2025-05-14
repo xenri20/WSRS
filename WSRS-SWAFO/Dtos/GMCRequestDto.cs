630ff9e0168fbd50d5a27f7c38a310e811e43275
xenri20 <kennethsoria20@gmail.com>
2025-05-14 23:30:50 +0800

    feat: converted to dto from vm

---
+++ WSRS-SWAFO/Dtos/GMCRequestDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WSRS_SWAFO.Data.Enum;
+
+namespace WSRS_SWAFO.Dtos
+{
+    public class GMCRequestDto
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Application Date")]
+        public DateOnly ApplicationDate { get; set; }
+        public string Purpose { get; set; }
+        public string Copies { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+        public string Course { get; set; }
+        [Display(Name = "Student Number")]
+        public int StudentNumber { get; set; }
+        public Gender Gender { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
