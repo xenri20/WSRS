c0166e9196032ac0e0ff60ed9b04f87a6203ac68
xenri20 <kennethsoria20@gmail.com>
2025-05-14 18:18:42 +0800

    feat: added post action for gmc request

--- WSRS-Student/Controllers/GMCController.cs
+++ WSRS-Student/Controllers/GMCController.cs
@@ -28,5 +28,42 @@ public class GMCController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> PostGMCRequest(GoodMoralRequestViewModel model)
+    {
+        var currentUser = await _userManager.GetUserAsync(User);
+        if (currentUser == null)
+        {
+            ModelState.AddModelError(string.Empty, "Unable to retrieve info of current user.");
+            return View(model);
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var client = _httpClientFactory.CreateClient("WSRS_Api");
+
+        var response = await client.PostAsJsonAsync("Gmc", model);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadFromJsonAsync<GoodMoralRequestViewModel>();
+
+            SetToastMessage("Your report has been submitted, thank you.", title: "Success");
+            return RedirectToAction(nameof(Index));
+        }
+
+        SetToastMessage("Something went wrong submitting your request.", title: "Error", cssClassName: "bg-danger text-white");
+        var errorContent = await response.Content.ReadAsStringAsync();
+        _logger.LogError("API Response: {StatusCode}, Content: {Content}", response.StatusCode, errorContent);
+
+        return RedirectToAction(nameof(Index));
+    }
 }
 
