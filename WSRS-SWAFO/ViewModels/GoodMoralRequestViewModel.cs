f2f8aefb295c716236aa30d0e9e138a80ccd9da2
xenri20 <kennethsoria20@gmail.com>
2025-05-14 23:31:15 +0800

    refactor: update gmc view model to dto ienumerable

--- WSRS-SWAFO/ViewModels/GoodMoralRequestViewModel.cs
+++ WSRS-SWAFO/ViewModels/GoodMoralRequestViewModel.cs
@@ -1,24 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using WSRS_SWAFO.Data.Enum;
+using WSRS_SWAFO.Dtos;
 
 namespace WSRS_SWAFO.ViewModels
 {
     public class GoodMoralRequestViewModel
     {
-        public int Id { get; set; }
-
-        [Display(Name = "Application Date")]
-        public DateOnly ApplicationDate { get; set; }
-        public string Purpose { get; set; }
-        public string Copies { get; set; }
-        [Display(Name = "Full Name")]
-        public string FullName { get; set; }
-        public string Course { get; set; }
-        [Display(Name = "Student Number")]
-        public int StudentNumber { get; set; }
-        public Gender Gender { get; set; }
-        [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
-        public bool IsApproved { get; set; }
+        public IEnumerable<GMCRequestDto> GMCRequests { get; set; }
     }
 }
