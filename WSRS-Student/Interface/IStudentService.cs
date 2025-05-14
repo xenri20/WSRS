579bbb526ff4f8ae3dd5d314352a2ba89e6c059d
xenri20 <kennethsoria20@gmail.com>
2025-05-14 22:38:03 +0800

    feat: added an interface for StudentService

---
+++ WSRS-Student/Interface/IStudentService.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace WSRS_Student.Interface
+{
+    public interface IStudentService
+    {
+        Task<bool> HandleStudentStatus(ClaimsPrincipal User);
+    }
+}
