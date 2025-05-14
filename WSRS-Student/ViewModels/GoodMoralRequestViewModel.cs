4f848c3b3ff1283202215e33f1d45aab96bcf39f
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-05-14 19:31:12 +0800

    chore(gmc): add viewmodel data annotations

--- WSRS-Student/ViewModels/GoodMoralRequestViewModel.cs
+++ WSRS-Student/ViewModels/GoodMoralRequestViewModel.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using System.ComponentModel.DataAnnotations;
 using WSRS_Student.Data.Enum;
 
 namespace WSRS_Student.ViewModels
@@ -6,14 +6,30 @@ namespace WSRS_Student.ViewModels
     public class GoodMoralRequestViewModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "Application Date")]
         public DateOnly ApplicationDate { get; set; }
         public string Purpose { get; set; }
         public string Copies { get; set; }
+
+        [Required]
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+
+        [Required]
         public string Course { get; set; }
+
+        [Required]
+        [Display(Name = "Student Number")]
         public int StudentNumber { get; set; }
+
+        [Required]
         public Gender Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+
         public bool IsApproved { get; set; }
     }
 }
